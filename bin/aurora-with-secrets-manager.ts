f3caf340416dbc6c3d35e63f9b2360e5f264b0ac
osawa-koki <koki@osawa.dev>
2024-11-24 00:30:36 +0900

    ネットワーク系リソースのプロビジョニング。

--- bin/aurora-with-secrets-manager.ts
+++ bin/aurora-with-secrets-manager.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+
 import { AuroraWithSecretsManagerStack } from '../lib/aurora-with-secrets-manager-stack';
+import { BASE_STACK_NAME } from '../lib/const';
 
 const app = new cdk.App();
-new AuroraWithSecretsManagerStack(app, 'AuroraWithSecretsManagerStack', {
+new AuroraWithSecretsManagerStack(app, BASE_STACK_NAME, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/aurora-with-secrets-manager-stack.ts
+++ lib/aurora-with-secrets-manager-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import NetworkStack from './network/network';
+import { BASE_STACK_NAME } from './const';
 
 export class AuroraWithSecretsManagerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuroraWithSecretsManagerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new NetworkStack(this, 'NetworkStack', {
+      stackName: `${BASE_STACK_NAME}-network`,
+    });
   }
 }
