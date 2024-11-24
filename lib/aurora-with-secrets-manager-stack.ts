e25f5fb7721d67a566b41e53fa7b535b4a1b64a9
osawa-koki <koki@osawa.dev>
2024-11-24 12:47:39 +0900

    CDワークフローの実装。

--- lib/aurora-with-secrets-manager-stack.ts
+++ lib/aurora-with-secrets-manager-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
 
 import NetworkStack from './network/network';
 import { BASE_STACK_NAME } from './const';
+
+dotenv.config();
 
 export class AuroraWithSecretsManagerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
